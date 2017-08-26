1506855c51fe02b97634f0f53d854e4fdce30d14
Jamie Jennings <jamiejennings@users.noreply.github.com>
2017-08-26 13:32:06 -0400

    Added very basic exceptions

--- thread.lua
+++ thread.lua
@@ -15,24 +15,63 @@ function thread.is(obj)
    return type(obj)=="thread"
 end
 
--- apply f to args until it returns or yields
+function thread.current()
+   return co.running()
+end
+
+-- thread.pcall: apply f to args until it returns or yields
 -- like pcall, return a success code in front of return values
-function thread.call(f, ...)
+function thread.pcall(f, ...)
    local t = co.create(f)
    return co.resume(t, ...)
 end
 
-function thread.current()
-   return co.running()
+local function thread_error(msg, ...)
+   error(table.concat({msg, "Values:", ...}, "\n"), 3)
 end
 
 function thread.yield(...)
    if co.isyieldable() then
       co.yield(...)
    else
-      return ...
+      thread_error("Cannot yield from main thread", ...)
    end
 end
+
+---------------------------------------------------------------------------------------------------
+-- Basic exceptions
+---------------------------------------------------------------------------------------------------
+
+local exception_tag = {__tostring = function(obj) return "<exception>" end}
+local function make_exception(...)
+   return setmetatable({...}, exception_tag)
+end
+local function is_exception(obj)
+   return (getmetatable(obj) == exception_tag)
+end
+
+thread.exception = {new = make_exception,
+		    is = is_exception }
+
+function thread.raise(...)
+   if coroutine.isyieldable() then
+      coroutine.yield(make_exception(...))
+   else
+      thread_error("Uncaught exception", ...)
+   end
+end
+
+function thread.throw(...)
+   if coroutine.isyieldable() then
+      coroutine.yield(...)
+   else
+      thread_error("Uncaught throw", ...)
+   end
+end
+
+---------------------------------------------------------------------------------------------------
+-- Thread-local storage
+---------------------------------------------------------------------------------------------------
 
 -- Thread-local variables are stored on a per-thread basis in a weak table.  When the thread is
 -- collected, that thread's vars will be collected on the next gc.
@@ -48,7 +87,7 @@ end
 
 local function set(threadvars, name, value)
    if type(name)~="string" then
-      error("thread variable name not a string: " .. tostring(name))
+      thread_error("thread variable name not a string: " .. tostring(name))
    end
    local t = thread.current()
    local locals = rawget(threadvars, t)
